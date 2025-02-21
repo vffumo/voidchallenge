12100601ae364d9beffe9800266fb7b39ab20739
Vayile Fumo <v.fumo@manifoldus.com>
2025-02-22 00:00:29 +0200

    finished Table1, added home

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import {BrowserRouter as Router,Routes, Route } from "react-router-dom";
-import Home from './page/HomePage';
+import Home from './pages/HomePage';
 import AnaliseProgresso from "./pages/AnaliseProgresso";
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css'
