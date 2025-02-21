67c933025805a887b6e10dde7b8852e0592bc10f
Vayile Fumo <v.fumo@manifoldus.com>
2025-02-22 00:15:21 +0200

    finished Table1, fixed home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,22 +1,26 @@
 // src/components/Home.jsx
 import React, { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';''
 import { Link } from 'react-router-dom';
 import api from '../services/api';
 
 const Home = () => {
-  const { user } = useSelector((state) => state.auth);
+//   const { user } = useSelector((state) => state.auth);
  
   return (
-    <div>
-      <h2>Welcome,</h2>
-      <h3>voi_challenge</h3>
-      <ul>
-      <li>
-            <Link to={`/analise-progresso/`}>Análise - Progresso</Link>
-            <Link to={`/insumos/`}>Insumos</Link>
-      </li>
-      </ul>
+    <div className="page-container text-left-all">
+      <div className="w-100 mt-1">
+        <h1 className='mb-3'>Welcome,</h1>
+        <p>:: void_challenge</p>
+        <ul className='mt-5'> 
+        <li className='mb-2'>
+                <Link to={`/analise-progresso/`}> {'>'} Análise - Progresso</Link>
+        </li>
+        <li>
+                <Link to={`/insumos/`}> {'>'} Insumos</Link>
+        </li>
+        </ul>
+      </div>
     </div>
   );
 };
